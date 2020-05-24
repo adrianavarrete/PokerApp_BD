817a41ce5b6e02ab7e2e93d06c8c3150b4cb88d4
adrianavarrete <adri.navarrete.09@gmail.com>
2020-05-24 14:08:46 +0200

    Repartimos la mano al jugador y decide si seguir o para, falta implementación con la API para ver las probabilidades

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,11 +11,16 @@ export class HomePage {
   baraja = []
   mano = []
 
-  card1:string = "../../assets/cards/back.png"
-  card2:string = "../../assets/cards/back.png"
-  card3:string = "../../assets/cards/back.png"
-  card4:string = "../../assets/cards/back.png"
-  card5:string = "../../assets/cards/back.png"
+  disabled1 = false
+  disabled2 = true
+  disabled3 = true
+
+
+  card1: string = "../../assets/cards/back.png"
+  card2: string = "../../assets/cards/back.png"
+  card3: string = "../../assets/cards/back.png"
+  card4: string = "../../assets/cards/back.png"
+  card5: string = "../../assets/cards/back.png"
 
 
 
@@ -23,33 +28,79 @@ export class HomePage {
   constructor() { }
 
   play() {
-    
-    this.mano = [];
+
     this.generarBaraja();
 
 
-    let card1 = this.randomCard();
-    let card2 = this.randomCard();
+    this.reparte();
+    this.reparte();
 
     this.card1 = this.desvelarCarta(this.mano[0])
     this.card2 = this.desvelarCarta(this.mano[1])
 
-    
+    this.disabled1 = true;
+    this.disabled2 = false;
+    this.disabled3 = false;
+
+
 
     console.log(this.mano[0]);
     console.log(this.mano[1]);
 
-    
+
 
   }
 
-  desvelarCarta(card: string){
+  next() {
+    if (this.mano.length == 2) {
+
+      this.reparte();
+      this.card3 = this.desvelarCarta(this.mano[2])
+
+    } else if (this.mano.length == 3) {
+
+      this.reparte();
+      this.card4 = this.desvelarCarta(this.mano[3])
+
+    } else if (this.mano.length == 4) {
+
+      this.reparte();
+      this.card5 = this.desvelarCarta(this.mano[4])
+
+      this.disabled1 = true;
+      this.disabled2 = true;
+      this.disabled3 = false;
+
+    } else {
+      console.log("finish")
+    }
+
+
+  }
+
+  stop() {
+    this.mano = [];
+    this.baraja = [];
+
+    this.card1 = "../../assets/cards/back.png"
+    this.card2 = "../../assets/cards/back.png"
+    this.card3 = "../../assets/cards/back.png"
+    this.card4 = "../../assets/cards/back.png"
+    this.card5 = "../../assets/cards/back.png"
+
+    this.disabled1 = false;
+    this.disabled2 = true;
+    this.disabled3 = true;
+
+  }
+
+  desvelarCarta(card: string) {
 
     let result;
-    
-    if(card.length != 3){
+
+    if (card.length != 3) {
       result = "../../assets/cards/" + card[0] + "_" + card[1] + ".png"
-    }else{
+    } else {
       result = "../../assets/cards/" + card[0] + "_" + card[1] + card[2] + ".png"
     }
 
@@ -72,7 +123,7 @@ export class HomePage {
     }
   }
 
-  randomCard(){
+  reparte() {
 
     let random = this.randomIntFromInterval(0, this.baraja.length - 1)
     let card = this.baraja[random]
@@ -81,7 +132,6 @@ export class HomePage {
 
     this.baraja.splice(random, 1)
 
-    return card
   }
 
   randomIntFromInterval(min, max) { // min and max included
